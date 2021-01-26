187c66ea638bd46197996cbe28d61aa724a2ea39
Gonca Gül Kazancı <gnc.kazanci@gmail.com>
2021-01-26 13:08:30 +0300

    Metots

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customerList = new List<Customer>();
+       
+        public void Add(Customer customer)
+        {
+
+            Console.WriteLine("Yeni Müşteri: " + customer.Name + " " + customer.Surname + " eklendi.");
+            customerList.Add(customer);
+        }
+
+        public void List()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(" ------ MÜŞTERİ LİSTESi ------ ");
+            foreach (var customerItem in customerList)
+                Console.WriteLine(customerItem.CreditCardNumber + " -  " + customerItem.Name + " " + customerItem.Surname);
+
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " müşterisi silindi.");
+        }
+
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassMetotDemo
 {
@@ -8,7 +9,33 @@ namespace ClassMetotDemo
         {
             try
             {
-                Console.WriteLine("Müşteri Adı");
+                Customer customer1 = new Customer();
+                customer1.Id = 1;
+                customer1.Name = "Ali";
+                customer1.Surname = "Kaya";
+                customer1.DateOfBirth = Convert.ToDateTime("31.08.1993");
+                customer1.City = "Ankara";
+                customer1.CreditCardNumber = "7382 6748 4939 3111";
+
+                Customer customer2 = new Customer();
+                customer2.Id = 1;
+                customer2.Name = "Veli";
+                customer2.Surname = "Çetin";
+                customer2.DateOfBirth = Convert.ToDateTime("31.08.1993");
+                customer2.City = "İstanbul";
+                customer2.CreditCardNumber = "5566 0029 4939 8490";
+
+
+
+                CustomerManager customerManager = new CustomerManager();
+                customerManager.Add(customer1);
+                customerManager.Add(customer2);
+
+               
+                customerManager.List();
+
+
+                Console.Read();
             }
             catch (Exception ex)
             {
